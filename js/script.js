7b2091a510768263ab9bfae3e43b8e371688b7c7
GTM-Winter <dimastriana03@gmail.com>
2022-04-15 14:46:04 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav, {
+      onCloseStart: true
+    });
+    
+    
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+   
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      hoverEnabled: false
+    });
+  });
+  function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+  }
+  function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+  }
